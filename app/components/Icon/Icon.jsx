36663da28e2325c8cadcb39570d90c0baca58899
weirdolando <93187436+weirdolando@users.noreply.github.com>
2023-09-09 23:59:03 +0700

    add hamburger menu

--- app/components/Icon/Icon.jsx
+++ app/components/Icon/Icon.jsx
@@ -1,12 +1,13 @@
 import Image from "next/image";
 import styled from "styled-components";
-import { Search, Bookmark, Info, ChevronLeft } from "react-feather";
+import { Search, Bookmark, Info, ChevronLeft, Menu } from "react-feather";
 
 import Pokeball from "@/app/assets/icons/pokeball.svg";
 import Heart from "@/app/assets/icons/heart.svg";
 import HeartSolid from "@/app/assets/icons/heart-solid.svg";
 
 const icons = {
+  menu: Menu,
   search: Search,
   info: Info,
   pokeball: Pokeball,
--- app/components/MainHeader/MainHeader.jsx
+++ app/components/MainHeader/MainHeader.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
 
@@ -7,10 +8,12 @@ import SearchInput from "../SearchInput";
 import UnstyledButton from "../UnstyledButton";
 import { useAuthContext } from "@/app/context/AuthContext";
 import signOut from "@/app/firebase/auth/signOut";
+import Icon from "../Icon/Icon";
 
 export default function MainHeader() {
   const { user } = useAuthContext();
   const { push } = useRouter();
+  const [showMenu, setShowMenu] = useState(false);
 
   function handleKeyDown(e) {
     if (e.key === "Enter") {
@@ -29,18 +32,39 @@ export default function MainHeader() {
     <Wrapper>
       <ContentWrapper className="screen-container">
         <SearchInput placeholder="Search Pokemon" onKeyDown={handleKeyDown} />
-        {user && (
-          <FlexWrapper>
-            <UserName>Hi, {user.displayName}</UserName>
-            <SignOutButton onClick={handleSignOut}>Sign Out</SignOutButton>
-          </FlexWrapper>
-        )}
-        {!user && (
-          <FlexWrapper>
-            <SignInButton href="/signin">Sign In</SignInButton>
-            <RegisterButton href="/register">Register</RegisterButton>
-          </FlexWrapper>
-        )}
+        <MenuButton onClick={() => setShowMenu(!showMenu)}>
+          <Icon id="menu" size={24} />
+        </MenuButton>
+
+        <MobileMenuWrapper style={{ "--left": showMenu ? "0" : "-100%" }}>
+          <MenuItemsWrapper className="screen-container">
+            {user ? (
+              <>
+                <UserName>👋 Hi, {user.displayName}</UserName>
+                <SignOutButton onClick={handleSignOut}>Sign Out</SignOutButton>
+              </>
+            ) : (
+              <>
+                <SignInButton href="/signin">Sign In</SignInButton>
+                <RegisterButton href="/register">Register</RegisterButton>
+              </>
+            )}
+          </MenuItemsWrapper>
+        </MobileMenuWrapper>
+
+        <FlexWrapper>
+          {user ? (
+            <>
+              <UserName>👋 Hi, {user.displayName}</UserName>
+              <SignOutButton onClick={handleSignOut}>Sign Out</SignOutButton>
+            </>
+          ) : (
+            <>
+              <SignInButton href="/signin">Sign In</SignInButton>
+              <RegisterButton href="/register">Register</RegisterButton>
+            </>
+          )}
+        </FlexWrapper>
       </ContentWrapper>
     </Wrapper>
   );
@@ -49,38 +73,19 @@ export default function MainHeader() {
 const Wrapper = styled.header`
   border-bottom: 1px solid ${COLORS.light["900"]};
   font-size: 1.125rem;
+  min-height: 65px;
   font-weight: ${WEIGHTS.medium};
   font-size: ${15 / 16}rem;
-
   color: ${COLORS.gray["900"]};
-`;
-
-const UserName = styled.div`
-  max-width: 150px;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  text-wrap: nowrap;
-
-  &::after {
-    content: "";
-    border-right: 1px solid ${COLORS.gray[500]};
-    margin-left: 12px;
-  }
+  position: relative;
 `;
 
 const ContentWrapper = styled.div`
   display: flex;
-  align-items: baseline;
+  align-items: center;
   justify-content: space-between;
   padding-block: 18px;
-  flex-wrap: wrap;
-  gap: 14px;
-`;
-
-const FlexWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 12px;
+  gap: 4px;
 `;
 
 const BaseLink = styled(Link)`
@@ -91,6 +96,7 @@ const BaseLink = styled(Link)`
 
 const SignInButton = styled(BaseLink)`
   color: ${COLORS.primary[600]};
+  background-color: ${COLORS.light[700]};
   border: 1px solid currentColor;
 `;
 
@@ -104,4 +110,55 @@ const SignOutButton = styled(UnstyledButton)`
   border-radius: 4px;
   color: ${COLORS.secondary[800]};
   border: 1px solid currentColor;
+  text-align: center;
+  position: relative;
+`;
+
+const UserName = styled.div`
+  max-width: 100%;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  text-wrap: nowrap;
+
+  @media (min-width: 500px) {
+    max-width: 150px;
+  }
+`;
+
+const FlexWrapper = styled.div`
+  display: none;
+
+  @media (min-width: 500px) {
+    display: flex;
+    align-items: center;
+    gap: 12px;
+  }
+`;
+
+const MenuButton = styled(UnstyledButton)`
+  @media (min-width: 500px) {
+    display: none;
+  }
+`;
+
+const MobileMenuWrapper = styled.div`
+  position: absolute;
+  background-color: ${COLORS.light[700]};
+  width: 100%;
+  top: 65px;
+  left: var(--left);
+  padding-block: 20px;
+  transition: all 300ms ease;
+  z-index: 10;
+
+  @media (min-width: 500px) {
+    display: none;
+  }
+`;
+
+const MenuItemsWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  text-align: center;
 `;
